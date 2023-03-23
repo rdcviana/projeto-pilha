a5408879c203ec7496bc7acb074215e088bdb945
Rodrigo Viana <rodrigodcviana@gmail.com>
2023-03-23 11:24:25 -0300

    Implementação de Pilha em Java

---
+++ src/main/java/one/digitalinovation/Pilha.java
@@ -0,0 +1,33 @@
+package one.digitalinovation;
+
+public class Pilha {
+
+    private No refNoEntradaPilha;
+
+    public Pilha() {
+        this.refNoEntradaPilha = null;
+    }
+
+    public boolean isEmpty() {
+        return refNoEntradaPilha == null ? true : false;
+    }
+
+    public No top() {
+        return refNoEntradaPilha;
+    }
+
+    public void push(No novoNo) {
+        No refAuxilar = refNoEntradaPilha;
+        refNoEntradaPilha = novoNo;
+        refNoEntradaPilha.setReferenciaNo(refAuxilar);
+    }
+
+    public No pop() {
+        if (!isEmpty()) {
+            No noPoped = refNoEntradaPilha;
+            refNoEntradaPilha = refNoEntradaPilha.getReferenciaNo();
+            return noPoped;
+        }
+        return null;
+    }
+}
